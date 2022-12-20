bf2ab37062f6f0583eef5be2a0ef034a98566dd9
Kathirvel15 <kathirvel.s@thecareerlabs.com>
2022-12-20 11:59:21 +0530

    Demo Gre Page Update

---
+++ gre.js
@@ -0,0 +1,241 @@
+document
+  .querySelector(".gre-section-4-show-section")
+  .addEventListener("click", function () {
+    var grehide = document.querySelector(".gre-section-4-hide-section");
+    if (grehide.style.display == "block") {
+      grehide.style.display = "none";
+      document.querySelector(".gre-section-4-inner-border-down").style.display =
+        "block";
+      document.querySelector(".gre-section-4-inner-border-up").style.display =
+        "none";
+    } else {
+      grehide.style.display = "block";
+      document.querySelector(".gre-section-4-inner-border-up").style.display =
+        "block";
+      document.querySelector(".gre-section-4-inner-border-down").style.display =
+        "none";
+    }
+  });
+// Column -2
+document
+  .querySelector(".gre-section-4-show-section-1")
+  .addEventListener("click", function () {
+    var grehide1 = document.querySelector(".gre-section-4-hide-section-1");
+    if (grehide1.style.display == "block") {
+      grehide1.style.display = "none";
+      document.querySelector(".gre-section-4-down-1").style.display = "block";
+      document.querySelector(".gre-section-4-up-1").style.display = "none";
+    } else {
+      grehide1.style.display = "block";
+      document.querySelector(".gre-section-4-up-1").style.display = "block";
+      document.querySelector(".gre-section-4-down-1").style.display = "none";
+    }
+  });
+
+//   Column - 3
+document
+  .querySelector(".gre-section-4-show-section-2")
+  .addEventListener("click", function () {
+    var grehide2 = document.querySelector(".gre-section-4-hide-section-2");
+    if (grehide2.style.display == "block") {
+      grehide2.style.display = "none";
+      document.querySelector(".gre-section-4-down-2").style.display = "block";
+      document.querySelector(".gre-section-4-up-2").style.display = "none";
+    } else {
+      grehide2.style.display = "block";
+      document.querySelector(".gre-section-4-up-2").style.display = "block";
+      document.querySelector(".gre-section-4-down-2").style.display = "none";
+    }
+  });
+// Column - 4
+document
+  .querySelector(".gre-section-4-show-section-3")
+  .addEventListener("click", function () {
+    var grehide3 = document.querySelector(".gre-section-4-hide-section-3");
+    if (grehide3.style.display == "block") {
+      grehide3.style.display = "none";
+      document.querySelector(".gre-section-4-down-3").style.display = "block";
+      document.querySelector(".gre-section-4-up-3").style.display = "none";
+    } else {
+      grehide3.style.display = "block";
+      document.querySelector(".gre-section-4-up-3").style.display = "block";
+      document.querySelector(".gre-section-4-down-3").style.display = "none";
+    }
+  });
+// Column - 5
+document
+  .querySelector(".gre-section-4-show-section-4")
+  .addEventListener("click", function () {
+    var grehide4 = document.querySelector(".gre-section-4-hide-section-4");
+    if (grehide4.style.display == "block") {
+      grehide4.style.display = "none";
+      document.querySelector(".gre-section-4-down-4").style.display = "block";
+      document.querySelector(".gre-section-4-up-4").style.display = "none";
+    } else {
+      grehide4.style.display = "block";
+      document.querySelector(".gre-section-4-up-4").style.display = "block";
+      document.querySelector(".gre-section-4-down-4").style.display = "none";
+    }
+  });
+// Column - 6
+document
+  .querySelector(".gre-section-4-show-section-5")
+  .addEventListener("click", function () {
+    var grehide5 = document.querySelector(".gre-section-4-hide-section-5");
+    if (grehide5.style.display == "block") {
+      grehide5.style.display = "none";
+      document.querySelector(".gre-section-4-down-5").style.display = "block";
+      document.querySelector(".gre-section-4-up-5").style.display = "none";
+    } else {
+      grehide5.style.display = "block";
+      document.querySelector(".gre-section-4-up-5").style.display = "block";
+      document.querySelector(".gre-section-4-down-5").style.display = "none";
+    }
+  });
+//   Column  - 7
+document
+  .querySelector(".gre-section-4-show-section-6")
+  .addEventListener("click", function () {
+    var grehide6 = document.querySelector(".gre-section-4-hide-section-6");
+    if (grehide6.style.display == "block") {
+      grehide6.style.display = "none";
+      document.querySelector(".gre-section-4-down-6").style.display = "block";
+      document.querySelector(".gre-section-4-up-6").style.display = "none";
+    } else {
+      grehide6.style.display = "block";
+      document.querySelector(".gre-section-4-up-6").style.display = "block";
+      document.querySelector(".gre-section-4-down-6").style.display = "none";
+    }
+  });
+// Column - 8
+document
+  .querySelector(".gre-section-4-show-section-7")
+  .addEventListener("click", function () {
+    var grehide7 = document.querySelector(".gre-section-4-hide-section-7");
+    if (grehide7.style.display == "block") {
+      grehide7.style.display = "none";
+      document.querySelector(".gre-section-4-down-7").style.display = "block";
+      document.querySelector(".gre-section-4-up-7").style.display = "none";
+    } else {
+      grehide7.style.display = "block";
+      document.querySelector(".gre-section-4-up-7").style.display = "block";
+      document.querySelector(".gre-section-4-down-7").style.display = "none";
+    }
+  });
+// Column - 9
+document
+  .querySelector(".gre-section-4-show-section-8")
+  .addEventListener("click", function () {
+    var grehide8 = document.querySelector(".gre-section-4-hide-section-8");
+    if (grehide8.style.display == "block") {
+      grehide8.style.display = "none";
+      document.querySelector(".gre-section-4-down-8").style.display = "block";
+      document.querySelector(".gre-section-4-up-8").style.display = "none";
+    } else {
+      grehide8.style.display = "block";
+      document.querySelector(".gre-section-4-up-8").style.display = "block";
+      document.querySelector(".gre-section-4-down-8").style.display = "none";
+    }
+  });
+// Column - 10
+document
+  .querySelector(".gre-section-4-show-section-9")
+  .addEventListener("click", function () {
+    var grehide9 = document.querySelector(".gre-section-4-hide-section-9");
+    if (grehide9.style.display == "block") {
+      grehide9.style.display = "none";
+      document.querySelector(".gre-section-4-down-9").style.display = "block";
+      document.querySelector(".gre-section-4-up-9").style.display = "none";
+    } else {
+      grehide9.style.display = "block";
+      document.querySelector(".gre-section-4-up-9").style.display = "block";
+      document.querySelector(".gre-section-4-down-9").style.display = "none";
+    }
+  });
+//Contact Form Functionality
+function namevalidation() {
+  var getname = document.querySelector("#name");
+  var geterror = document.querySelector(".name-error");
+  //icon
+  var getnameicon = document.querySelector(".nameicon");
+  if (getname.value == "") {
+    geterror.innerHTML = "Name is requried";
+    getname.style.border = "2px solid red";
+    getname.style.outline = "none";
+    getnameicon.innerHTML = "";
+    return false;
+  }
+  if (getname.value.length < 3) {
+    geterror.innerHTML = "Name is requried";
+    return false;
+  }
+  if (!getname.value.match(/^[a-zA-Z]/)) {
+    geterror.innerHTML = "Digits are not allowed";
+    getnameicon.innerHTML = "";
+    return false;
+  }
+  geterror.innerHTML = "";
+  getname.style.border = "2px solid #3e3c3cb2";
+  getnameicon.innerHTML =
+    '<img src="Assets/laptop-and-tick-shows-online-success.jpg" width="28px">';
+  return true;
+}
+// Email id Validation
+function emailvalidation() {
+  var getname1 = document.querySelector("#email");
+  var geterror1 = document.querySelector(".email-error");
+  //icon
+  var getnameicon1 = document.querySelector(".emailicon");
+  if (
+    !getname1.value.match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    )
+  ) {
+    geterror1.innerHTML = getname1.value + " is not valid";
+    getname1.style.border = "2px solid red";
+    getnameicon1.innerHTML = "";
+    return false;
+  }
+  geterror1.innerHTML = "";
+  getname1.style.border = "2px solid #3e3c3cb2";
+  getnameicon1.innerHTML =
+    '<img src="Assets/laptop-and-tick-shows-online-success.jpg" width="28px">';
+  return true;
+}
+// Phone Number
+function phonevalidation() {
+  var getname2 = document.querySelector("#phone");
+  var geterror2 = document.querySelector(".phone-error");
+  //icon
+  var getnameicon2 = document.querySelector(".phoneicon");
+  if (getname2.value.length < 10) {
+    geterror2.innerHTML = "10 digit phone number is valid";
+    getname2.style.border = "2px solid red";
+    getnameicon2.innerHTML = "";
+    return false;
+  }
+  geterror2.innerHTML = "";
+  getname2.style.border = "2px solid #3e3c3cb2";
+  getnameicon2.innerHTML =
+    '<img src="Assets/laptop-and-tick-shows-online-success.jpg" width="28px">';
+  return true;
+}
+function checkvali() {
+  var a = document.querySelector("#name").focus();
+  if (!namevalidation() || !emailvalidation() || !phonevalidation()) {
+    document.querySelector(".gre-section-6-submit-error").innerHTML =
+      "Please Fill the Form";
+    document.querySelector(".gre-section-6-submit-error").style.display =
+      "block";
+    setTimeout(checksubmitbtn, 3000);
+    function checksubmitbtn() {
+      document.querySelector(".gre-section-6-submit-error").style.display =
+        "none";
+    }
+    return false;
+  } else {
+    document.querySelector(".gre-section-6-submit-error").innerHTML = "";
+    window.location.href = "http://programminghead.com";
+    document.querySelector("form").reset();
+  }
+}
